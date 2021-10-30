d3f5bc7cb15218175ef55309d472774b16a6a6bf
FrancoPrime <franco28xl@gmail.com>
2021-10-30 18:25:00 -0300

    change name on tests

--- server/src/wildcard.rs
+++ server/src/wildcard.rs
@@ -34,55 +34,55 @@ mod tests {
     fn test01_prueba_wildcard_no_debe_matchear() {
         let topic = String::from("sport/tennis/picnic/paco");
         let subscription = String::from("sport/+/+");
-        assert!(!mqtt_wildcard(&topic, &subscription));
+        assert!(!compare_topic(&topic, &subscription));
     }
 
     #[test]
     fn test02_prueba_wildcard_debe_matchear() {
         let topic = String::from("sport/tennis/player1/ranking");
         let subscription = String::from("sport/tennis/player1/#");
-        assert!(mqtt_wildcard(&topic, &subscription));
+        assert!(compare_topic(&topic, &subscription));
     }
 
     #[test]
     fn test03_prueba_wildcard_debe_matchear() {
         let topic = String::from("sport/tennis/player1");
         let subscription = String::from("sport/tennis/player1/#");
-        assert!(mqtt_wildcard(&topic, &subscription));
+        assert!(compare_topic(&topic, &subscription));
     }
 
     #[test]
     fn test04_prueba_wildcard_debe_matchear() {
         let topic = String::from("sport/tennis/player1/score/wimbledon");
         let subscription = String::from("sport/tennis/player1/#");
-        assert!(mqtt_wildcard(&topic, &subscription));
+        assert!(compare_topic(&topic, &subscription));
     }
 
     #[test]
     fn test05_prueba_wildcard_debe_matchear() {
         let topic = String::from("sport/tennis/pijama/coconut");
         let subscription = String::from("sport/tennis/+/coconut");
-        assert!(mqtt_wildcard(&topic, &subscription));
+        assert!(compare_topic(&topic, &subscription));
     }
 
     #[test]
     fn test06_prueba_wildcard_debe_matchear() {
         let topic = String::from("sport/tennis/player2/coconut");
         let subscription = String::from("sport/tennis/+/coconut");
-        assert!(mqtt_wildcard(&topic, &subscription));
+        assert!(compare_topic(&topic, &subscription));
     }
 
     #[test]
     fn test07_prueba_wildcard_no_debe_matchear() {
         let topic = String::from("sport/tennis/player2/miranda/coconut");
         let subscription = String::from("sport/tennis/+/coconut");
-        assert!(!mqtt_wildcard(&topic, &subscription));
+        assert!(!compare_topic(&topic, &subscription));
     }
 
     #[test]
     fn test08_prueba_wildcard_no_debe_matchear() {
         let topic = String::from("sport/tennis");
         let subscription = String::from("sport/tennis/+");
-        assert!(!mqtt_wildcard(&topic, &subscription));
+        assert!(!compare_topic(&topic, &subscription));
     }
 }
